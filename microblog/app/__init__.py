bc62bc34c2b7f745ded6e6d14645a6c370a29c96
wenhua <17712676370@163.com>
2019-03-09 00:33:52 +0800

    Modify
    修改关闭端口

--- microblog/app/__init__.py
+++ microblog/app/__init__.py
@@ -4,5 +4,13 @@ app = Flask(__name__)
 
 from app import routes
 
-# if __name__ == '__main__':
-# 	app.run(host='0.0.0.0', port=80);
+if __name__ == '__main__':
+	app.run(host='0.0.0.0', port=8888);
+
+
+
+
+# 
+# 
+# kill -9 `lsof -i:5000 -t` 关闭5000端口
+
