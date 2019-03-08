3eba68cb051ceb195ffb3efecc2fda4e564391aa
wenhua <17712676370@163.com>
2019-03-09 02:52:39 +0800

    modify

--- microblog/app/forms.py
+++ microblog/app/forms.py
@@ -5,5 +5,5 @@ from wtforms.validators import DataRequired
 class LoginForm(FlaskForm):
     username = StringField('Username',validators=[DataRequired])
     password = PasswordField('Password',validators=[DataRequired])
-    remember_me = BooleanField('Remeber Me')
+    remeber_me = BooleanField('Remeber Me')
     submit = SubmitField('Sign In')
