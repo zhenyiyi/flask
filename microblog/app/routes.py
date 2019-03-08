40af45986580a5dd35010197a324101346b00156
wenhua <17712676370@163.com>
2019-03-09 00:19:07 +0800

    add host

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -6,5 +6,19 @@ from app import app
 
 def index():
 	return "Hello, World!"
+# 	user = {'username' : 'xiaoying'}
+
+# 	return '''
+# <!DOCTYPE html>
+# <html>
+# <head>
+# 	<title>Home Page - fenglin</title>
+# </head>
+# <body>
+# 	<h1>Hello, ''' + user['username'] + ''' !</h1>
+# </body>
+# </html> '''
+
+
 
 	
