7ec9f89c0799c4bef17f42c9ad78041c9640c76f
wenhua <17712676370@163.com>
2019-03-09 00:55:16 +0800

    Templates

--- microblog/app/__init__.py
+++ microblog/app/__init__.py
@@ -4,13 +4,15 @@ app = Flask(__name__)
 
 from app import routes
 
-if __name__ == '__main__':
-	app.run(host='0.0.0.0', port=8888);
-
-
-
 
 # 
 # 
 # kill -9 `lsof -i:5000 -t` 关闭5000端口
+#
+# 
+# 
+#  
+# flask run -p 8888 -h 0.0.0.0
+
+
 
--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -1,23 +1,16 @@
 from app import app
+from flask import render_template
+
 
 @app.route('/')
 
 @app.route('/index')
 
 def index():
-	return "Hello, World!"
-# 	user = {'username' : 'xiaoying'}
+	user = {'username' : 'xiaoying'}
+	return render_template('index.html', title='Home Page',user=user)
+	
 
-# 	return '''
-# <!DOCTYPE html>
-# <html>
-# <head>
-# 	<title>Home Page - fenglin</title>
-# </head>
-# <body>
-# 	<h1>Hello, ''' + user['username'] + ''' !</h1>
-# </body>
-# </html> '''
 
 
 
