0f9a87002cbca7ee716bce1daad2619dd926ad5b
wenhua <17712676370@163.com>
2019-03-09 02:32:57 +0800

    add block content

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -1,10 +1,14 @@
 from app import app
 from flask import render_template
+from app.forms import LoginForm
+
 
 
 @app.route('/')
 
 @app.route('/index')
+
+@app.route('/login')
 
 def index():
 	user = {'username' : 'yangyang'}
@@ -21,7 +25,9 @@ def index():
 	return render_template('index.html', title='Home Page',user=user, posts=posts)
 
 
-
+def login():
+	form = LoginForm()
+	return render_template('login.html', title='Sign In',form=form)
 
 
 	
