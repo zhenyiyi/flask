ff5774ab3f8c3c9c8ba9515a0d601c5f6729dbab
wenhua <17712676370@163.com>
2019-03-09 21:41:11 +0800

    Revert "Add flask shell"
    This reverts commit c202918c853b7ce65a67c93372973f6cfbf209d8.

--- microblog/microblog.py
+++ microblog/microblog.py
@@ -1,11 +1,2 @@
 from app import app
 
-from app.models import User, Post
-
-@app.shell_context_processor
-def make_shell_context():
-    return {
-        'db' : db,
-        'User' : User,
-        'Post' : Post
-    }
