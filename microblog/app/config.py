21282f5057d158441dcc74a118076e5e78d80b64
wenhua <17712676370@163.com>
2019-03-09 15:26:55 +0800

    pip3 install pymysql

--- microblog/app/config.py
+++ microblog/app/config.py
@@ -9,8 +9,9 @@ class Config(object):
 	SECRET_KEY = os.environ.get('SECRET_KEY') or '123456'
 
 	# url的格式为：数据库的协议： // 用户名：密码 @ ip地址：端口号（默认可以不写） / 数据库名
-	#                 mysql ://   root : QAZwsx123. @ localhost/app
-	SQLALCHEMY_DATABASE_URI = "mysql://root:QAZwsx123.@localhost/app"
+	#                 mysql ://   root + pymysql : QAZwsx123. @ localhost/app
+	# pip3 install pymysql
+	SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:QAZwsx123.@localhost/app"
 	SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
