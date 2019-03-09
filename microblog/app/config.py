c44253cf9a18932a73769f8457f68f08dde10868
wenhua <17712676370@163.com>
2019-03-09 13:45:36 +0800

    database

--- microblog/app/config.py
+++ microblog/app/config.py
@@ -1,5 +1,17 @@
 import os
 
+
+baseDir = os.path.abspath(os.path.dirname(__file__))
+
+
+
 class Config(object):
 	SECRET_KEY = os.environ.get('SECRET_KEY') or '123456'
+
+	SQLALCHEMY_DARABASE_URI = os.environ.get('DATABASE_URL') or \
+		'sqlite:///' + os.path.join(baseDir, 'app.db')
+
+	SQLALcHEMY_TRACE_MODIFICATIONS = False
+
+
 
